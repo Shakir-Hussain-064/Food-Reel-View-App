a7316c1ce76549f6cbebb874b26cffb005b7b9be
Shakir Hussain <shakirhusain2021@gmail.com>
2025-09-15 21:59:43 +0530

    Completed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());  //middleware hai jisse data read karta hai server
 app.use(cors({
-    origin: ['https://food-reel-view-app.onrender.com'], 
+    origin: ['https://food-reel-view-app-1.onrender.com'], 
     credentials: true
 }));
 
@@ -18,8 +18,8 @@ app.get('/', (req,res)=>{
     res.send("Hello World");
 })
 
-app.use('https://food-reel-view-app-1.onrender.com/api/auth', authRoutes);
-app.use('https://food-reel-view-app-1.onrender.com/api/food', foodRoutes);
-app.use('https://food-reel-view-app-1.onrender.com/api/food-partner', foodPartnerRoutes);
+app.use('https://food-reel-view-app.onrender.com/api/auth', authRoutes);
+app.use('https://food-reel-view-app.onrender.com/api/food', foodRoutes);
+app.use('https://food-reel-view-app.onrender.com/api/food-partner', foodPartnerRoutes);
 
 module.exports = app;  
